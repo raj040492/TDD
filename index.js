9b6b7614f3e47bc13f3d1d2b76796b581b2c225c
raj-kumar-litmus <132570273+raj-kumar-litmus@users.noreply.github.com>
2024-09-03 20:37:42 +0530

    [test] Sum of Array of strings

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 export const sum = (arr) =>
   arr.reduce((curr, acc) => {
-    acc += curr;
-    return acc;
+    if (Number(curr)) {
+      acc = Number(acc) + Number(curr);
+    }
+    return Number(acc);
   }, 0);
--- test/index.test.js
+++ test/index.test.js
@@ -3,4 +3,7 @@ describe("String calculator", () => {
   it("sums the array of numbers", () => {
     expect(sum([1, 2, 3])).toEqual(6);
   });
+  it("sums the array of strings", () => {
+    expect(sum(["1", "2", "3"])).toEqual(6);
+  });
 });
