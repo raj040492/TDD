53897e05bc0ee064ee07e3d0f2d2718bbef3b368
raj-kumar-litmus <132570273+raj-kumar-litmus@users.noreply.github.com>
2024-09-04 14:50:57 +0530

    [feat] Add README.md + jsDocs

---
+++ README.md
@@ -0,0 +1,14 @@
+# TDD Assessment
+
+The repo contains the solution for the [Incubyte](https://blog.incubyte.co/blog/tdd-assessment) assessment.
+
+## Installation
+
+The code was built with [Node.js](https://nodejs.org/) v18.
+
+Clone the repository and follow the code given below:
+
+```sh
+npm i
+npm run test
+```
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * Takes an array of stringified numbers and returns the sum of the numbers
+ * @param {Array} arr
+ * @returns number
+ */
 export const sum = (arr) =>
   arr.reduce((acc, curr) => {
     if (Number(curr)) {
@@ -6,16 +11,34 @@ export const sum = (arr) =>
     return Number(acc);
   }, 0);
 
+/**
+ * Takes in an array of stringified numbers and returns an array of stringified numbers that are less than zero
+ * @param {Array} arr
+ * @returns Array
+ */
 export const filterNegatives = (arr) => arr.filter((str) => Number(str) < 0);
 
+/**
+ * Takes in a string and returns a string that is embedded between \\ and \n
+ * @param {string} str
+ * @returns string
+ */
 export const isolateDelimiter = (str) => /\/\/(.*)\n/.exec(str)?.[1];
 
+/**
+ * Takes in a string of numbers with a delimiter embedded at the start and returns the sum of numbers
+ * @param {string} string
+ * @returns number
+ */
 export const add = (string) => {
   const delimiter = isolateDelimiter(string) || "\n";
   const negativeValues = filterNegatives(
     string.replaceAll("\n", ",").split(delimiter)
   );
   if (negativeValues.length) {
+    /**
+     * Negative numbers are not expected
+     */
     throw new Error(
       `negative numbers not allowed <${negativeValues.join(",")}>`
     );
