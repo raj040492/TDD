48556de713d08f975a2ea45c7a07a504dcb7347a
raj-kumar-litmus <132570273+raj-kumar-litmus@users.noreply.github.com>
2024-09-04 08:55:16 +0530

    [test] Dynamic delimiters in a string are identified and handled

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ export const sum = (arr) =>
 
 export const filterNegatives = (arr) => arr.filter((str) => Number(str) < 0);
 
-export const isolateDelimiter = (str) => /\/\/(.*)\n/.exec(str)[1];
+export const isolateDelimiter = (str) => /\/\/(.*)\n/.exec(str)?.[1];
 
 export const add = (string) => {
+  const delimiter = isolateDelimiter(string) || "\n";
   const negativeValues = filterNegatives(
-    string.replaceAll("\n", ",").split(",")
+    string.replaceAll("\n", ",").split(delimiter)
   );
   if (negativeValues.length) {
     throw new Error(
       `negative numbers not allowed <${negativeValues.join(",")}>`
     );
   }
-  return sum(string.replaceAll("\n", ",").split(","));
+  return sum(string.replaceAll("\n", delimiter).split(delimiter));
 };
--- test/index.test.js
+++ test/index.test.js
@@ -7,19 +7,30 @@ describe("String calculator", () => {
     expect(sum(["1", "2", "3"])).toEqual(6);
   });
   it("add returns the sum of strings", () => {
-    expect(add("1,2,3,4")).toEqual(10);
+    expect(add("//,\n1,2,3,4")).toEqual(10);
   });
-  it("new spaces are converted into commas and summed", () => {
+  it("new spaces are converted into delimters and summed", () => {
     expect(
-      add(`1
+      add(`//\n\n1
     2
     3
     4`)
     ).toEqual(10);
   });
+  it("Multiple newlines are converted into delimiter and summed", () => {
+    expect(
+      add(`//\n\n\n1
+
+    2
+
+    3
+    
+    4`)
+    ).toEqual(10);
+  });
   it("add returns error when negative numbers are passed", () => {
     try {
-      add("1,2,3,-4");
+      add("//,\n1,2,3,-4");
     } catch (err) {
       expect(err.message).toEqual("negative numbers not allowed <-4>");
     }
@@ -31,14 +42,17 @@ describe("String calculator", () => {
   });
   it("add returns error when multiple negative numbers are passed", () => {
     try {
-      add("1,2,3,-4,-5,-6,7");
+      add("//,\n1,2,3,-4,-5,-6,7");
     } catch (err) {
       expect(err.message).toEqual("negative numbers not allowed <-4,-5,-6>");
     }
   });
-  it("add returns the sum of strings", () => {
-    expect(isolateDelimiter("//;\n1;2")).toEqual(";");
-    expect(isolateDelimiter("//-\n1;2")).toEqual("-");
-    expect(isolateDelimiter("//@\n1;2")).toEqual("@");
+  it("extracts the delimiter", () => {
+    expect(isolateDelimiter("//;\n1;2;3")).toEqual(";");
+    expect(isolateDelimiter("//-\n1-2-3")).toEqual("-");
+    expect(isolateDelimiter("//@\n1@2@3")).toEqual("@");
+  });
+  it("add returns the sum of strings with a different delimiter", () => {
+    expect(add("//-\n1-2-3-4")).toEqual(10);
   });
 });
